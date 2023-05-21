c9cbee3529a794b1f9d315015365c7456587c940
Mcsvip <133824884+MasonGUOJSJAFH@users.noreply.github.com>
2023-05-21 23:17:55 +0800

    create global css

--- client_web/src/App.js
+++ client_web/src/App.js
@@ -4,7 +4,6 @@ import {
   Route,
 } from "react-router-dom";
 import { Link } from "react-router-dom";
-import StaffProfile from './page/StaffProfile';
 import EditProfile from './page/EditProfile';
 import StaffList from './page/StaffList';
 import StaffDetail from './page/StaffDetail';
@@ -13,16 +12,6 @@ import './App.css';
 import HomePage from './page/Home';
 
 function App() {
-  const mockData = {
-    name: 'John Doe',
-    title: 'Professor',
-    researchField: 'Computer Science',
-    research: ['Research 1', 'Research 2'],
-    papers: [
-      { title: 'Paper 1', downloadLink: '#' },
-      { title: 'Paper 2', downloadLink: '#' },
-    ],
-  };
   return (
     <div className="App">
       <header className="App-header">
@@ -39,7 +28,6 @@ function App() {
         <Routes>
           {/* <Route path="/" element={<App />} /> */}
           <Route path="/"  element={<StaffList/>} />
-          <Route path="/profile" element={<StaffProfile profile={mockData}/>} />
           <Route path="/edit/:id" element={<EditProfile/>} />
           <Route path="/staff/:id" element={<StaffDetail/>} />
           <Route path="/stafflist" element={<StaffList/>} />
