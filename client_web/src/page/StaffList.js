cbb788639a487db3c1578ee6af567b786c0d9e14
Mcsvip <133824884+MasonGUOJSJAFH@users.noreply.github.com>
2023-05-21 23:41:32 +0800

    Improve StaffList functionality and css layout

--- client_web/src/page/StaffList.js
+++ client_web/src/page/StaffList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './StaffList.css';
 
 function StaffList() {
     const [staffList, setStaffList] = useState([]);
@@ -13,13 +14,15 @@ function StaffList() {
     }, []);
 
     return (
-        <div>
+        <div className="staff-list">
             <h1>Staff List</h1>
             {staffList.map(staff => (
-                <div key={staff.id}>
-                    <h2><Link to={`/staff/${staff.id}`}>{staff.name}</Link></h2>
-                    <p>{staff.title}</p>
-                    <img src={staff.image_url} alt={staff.name} />
+                <div key={staff.id} className="staff-card">
+                    <img className="staff-image" src={staff.image_url} alt={staff.name} />
+                    <div className="staff-info">
+                        <h2><Link to={`/staff/${staff.id}`}>{staff.name}</Link></h2>
+                        <p>{staff.title}</p>
+                    </div>
                 </div>
             ))}
         </div>
