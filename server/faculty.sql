444efc679962590cdb6e783dc6e24b34ea1f738d
Mcsvip <133824884+MasonGUOJSJAFH@users.noreply.github.com>
2023-05-21 23:48:19 +0800

    The data is modified to the actual data

--- server/faculty.sql
+++ server/faculty.sql
@@ -1,3 +1,4 @@
+drop database facutl_database;
 create database facutl_database;
 use facutl_database;
 CREATE TABLE Staff (
@@ -26,13 +27,13 @@ CREATE TABLE Articles (
 
 
 INSERT INTO Staff (name, title, image_url) VALUES
-                                               ('John Doe', 'Professor', 'https://example.com/images/john_doe.jpg'),
-                                               ('Jane Smith', 'Lecturer', 'https://example.com/images/jane_smith.jpg');
+                                               ('Albert Einstein', 'Professor of Physics', 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg'),
+                                               ('Marie Curie', 'Professor of Chemistry', 'https://upload.wikimedia.org/wikipedia/commons/7/71/Marie_Curie_c1920.jpg');
 
 INSERT INTO Research (staff_id, research_title, research_summary) VALUES
-                                                                      (1, 'Research 1', 'Summary of research 1'),
-                                                                      (2, 'Research 2', 'Summary of research 2');
+                                                                      (1, 'Theory of Relativity', 'Investigation of the fundamentals of spacetime.'),
+                                                                      (2, 'Radioactivity', 'Discovery of radium and polonium, furthering understanding of atomic structure.');
 
 INSERT INTO Articles (staff_id, article_title, article_url) VALUES
-                                                                (1, 'Article 1', 'https://example.com/articles/article1.pdf'),
-                                                                (2, 'Article 2', 'https://example.com/articles/article2.pdf');
+                                                                (1, 'On the Electrodynamics of Moving Bodies', 'https://www.wikisource.org/wiki/On_the_Electrodynamics_of_Moving_Bodies'),
+                                                                (2, 'Radioactive Substances', 'https://www.gutenberg.org/ebooks/54206');
